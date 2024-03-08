bb0a85e9354c941716d6c5457742a24ea1e3f4bc
Elvan Hidayat <elvanhd@gmail.com>
2024-03-08 17:34:24 +0700

    Integrate Twig and enhance HelloController
    - Add Twig bundle and dependencies for template rendering
    - Update HelloController to extend AbstractController for Twig support
    - Replace direct Response objects with Twig template rendering in both
      index and showOne methods
    - Introduce templates for base layout, HelloController views, and partials
    - Utilize Twig syntax for dynamic content display and conditional formatting
    - Add template for displaying individual message details with creation date
    - Implement date-based conditional display logic in message partial template

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,26 +2,38 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     private array $messages = [
-        "Hello", "Hi", "Bye!"
+        ['message' => 'Hello', 'created' => '2022/06/12'],
+        ['message' => 'Hi', 'created' => '2022/04/12'],
+        ['message' => 'Bye!', 'created' => '2021/05/12']
     ];
 
     #[Route('/{limit?3}', name: 'app_index')]
     public function index(int $limit): Response
     {
-        return new Response(
-            implode(',', array_slice($this->messages, 0, $limit))
+        return $this->render(
+            'hello/index.html.twig',
+            [
+                'messages' => $this->messages,
+                'limit' => $limit
+            ]
         );
     }
 
     #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
     public function showOne(int $id): Response
     {
-        return new Response($this->messages[$id]);
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+        );
     }
 }
