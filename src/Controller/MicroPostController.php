cfb7435fbf8e7bcc6648eed2928eed07ce04312d
Elvan Hidayat <elvanhd@gmail.com>
2024-03-09 04:57:55 +0700

    Enhance MicroPost addition with form handling
    - Integrate form submission handling in MicroPostController add method
    - Use Symfony's Request object to handle form data submission
    - Automatically set the creation date for new MicroPosts upon submission
    - Save new MicroPost entities to the database after form validation
    - Implement flash messages for successful post addition feedback
    - Customize form rendering in the add.html.twig template for better UX
    - Display flash messages in the base layout for user feedback across pages

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,14 +30,27 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
-    public function add(): Response
+    public function add(Request $request, MicroPostRepository $posts): Response
     {
         $microPost = new MicroPost();
         $form = $this->createFormBuilder($microPost)
             ->add('title')
             ->add('text')
-            ->add('submit', SubmitType::class, ['label' => 'Save'])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->add($post, true);
+
+            // Add a flash
+            $this->addFlash('success', 'Your micro post have been addded.');
+
+            // Redirect
+            return $this->redirectToRoute('app_micro_post');
+        }
 
         return $this->renderForm(
             'micro_post/add.html.twig',
