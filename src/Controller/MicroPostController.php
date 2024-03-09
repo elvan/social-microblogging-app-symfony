acd10221e8b1a27b532210fc1564e49a8799e8b3
Elvan Hidayat <elvanhd@gmail.com>
2024-03-09 16:59:35 +0700

    Implement comments feature for MicroPosts
    - Enable users to add comments to MicroPosts via new form
    - Introduce CommentType form class for handling comment submission
    - Add validation constraints to Comment entity ensuring comments integrity
    - Update MicroPostController to handle comment form submission and display
    - Create comment.html.twig template for rendering comment form and related post
    - Enhance _post.html.twig with a link to the comment form for each post
    - Integrate frontend adjustments for displaying and adding comments

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\MicroPost;
+use App\Form\CommentType;
 use App\Form\MicroPostType;
+use App\Repository\CommentRepository;
 use App\Repository\MicroPostRepository;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -76,6 +79,36 @@ class MicroPostController extends AbstractController
             'micro_post/edit.html.twig',
             [
                 'form' => $form
+            ]
+        );
+    }
+
+    #[Route('/micro-post/{post}/comment', name: 'app_micro_post_comment')]
+    public function addComment(MicroPost $post, Request $request, CommentRepository $comments): Response
+    {
+        $form = $this->createForm(CommentType::class, new Comment());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+            $comment->setPost($post);
+            $comments->add($comment, true);
+
+            // Add a flash
+            $this->addFlash('success', 'Your comment have been updated.');
+
+            // Redirect
+            return $this->redirectToRoute(
+                'app_micro_post_show',
+                ['post' => $post->getId()]
+            );
+        }
+
+        return $this->renderForm(
+            'micro_post/comment.html.twig',
+            [
+                'form' => $form,
+                'post' => $post
             ]
         );
     }
