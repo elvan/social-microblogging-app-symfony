40e888bf902dca5d66b3784d14dbec51156bb5c5
Elvan Hidayat <elvanhd@gmail.com>
2024-03-12 17:51:22 +0700

    Introduce extra privacy feature for micro-posts
    - Implement database migration to add extra_privacy boolean field to
      micro_post entity for enhanced privacy control
    - Update MicroPost entity to include extraPrivacy attribute with getter
      and setter methods
    - Modify MicroPostType form to include extraPrivacy toggle field
    - Enhance CommentType form by specifying TextareaType for text field
    - Adjust MicroPostVoter to restrict VIEW attribute based on extraPrivacy
      setting and user relationships (author or follows)
    - Update micro-post form template to display extra privacy option with
      a descriptive message about its impact
    - Introduce TextareaType in forms for improved user input experience

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,7 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('text');
+            ->add('text', TextareaType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
